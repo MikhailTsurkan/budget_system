1eda4e96bb2d8fa00442e6b41854aa2c9efc3624
Maxim Miknevichus <mmiknevichus@mail.ru>
2024-07-03 16:01:18 +0300

    add initial version of enrollment and withdrawal

--- store/notions.py
+++ store/notions.py
@@ -12,3 +12,15 @@ class Notion:
 
     def json(self, dumped=True):
         return j.dumps(self.__dict__) if dumped else self.__dict__
+
+
+class Enrollment(Notion):
+    def __init__(self, _sum, _data, _method):
+        super().__init__(_sum, _data, _method)
+        self.type = Enrollment
+
+
+class Withdrawal(Notion):
+    def __init__(self, _sum, _data, _method):
+        super().__init__(_sum, _data, _method)
+        self.type = Withdrawal
